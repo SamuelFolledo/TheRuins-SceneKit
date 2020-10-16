e5b1eccc66f808b81b15fa93c8adbf454472356f
Samuel Folledo <samuelfolledo@gmail.com>
2020-10-15 23:37:33 -0400

    [add] setup character node

--- TheRuins/Nodes/Player.swift
+++ TheRuins/Nodes/Player.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2020 SamuelFolledo. All rights reserved.
 //
 
-import Foundation
+import SceneKit
+
+class Player: SCNNode {
+    
+    private var daeHolderNode = SCNNode()
+    private var characterNode: SCNNode!
+    
+    //MARK: Initialization
+    override init() {
+        super.init()
+        setupModel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: Scene
+    func setupModel() {
+        //load dae childs
+        let playerUrl = Bundle.main.url(forResource: "art.scnassets/Scenes/Hero/idle", withExtension: "dae")
+        let playerScene = try! SCNScene(url: playerUrl!, options: nil)
+        for child in playerScene.rootNode.childNodes {
+            daeHolderNode.addChildNode(child)
+        }
+        addChildNode(daeHolderNode)
+    }
+}
